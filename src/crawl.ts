f2a2b78b6cc8049e8752592a01791cfd077cd251
Przemek Lewicki <przemyslaw.lewicki.dev@proton.me>
2026-08-30 22:49:54 +0200

    Add getHeadingFromHTML function with tests

--- src/crawl.ts
+++ src/crawl.ts
@@ -1,3 +1,6 @@
+import { JSDOM } from "jsdom";
+import { resourceLimits } from "node:worker_threads";
+
 export function normalizeURL(url: string): string | undefined {
   try {
     const parsed = new URL(url);
@@ -9,4 +12,23 @@ export function normalizeURL(url: string): string | undefined {
     console.error(`Cannot parse URL: ${url}; Error: ${e}`);
     return undefined;
   }
+}
+
+export function getHeadingFromHTML(html: string): string {
+  const dom = new JSDOM(html);
+  let result = "";
+
+  const h1s = dom.window.document.querySelectorAll("h1");
+  if (h1s.length == 0) {
+    const h2s = dom.window.document.querySelectorAll("h2");
+    if (h2s.length == 0) {
+      result = "";
+    } else {
+      result = h2s[0].textContent;
+    }
+  } else {
+    result = h1s[0].textContent;
+  }
+
+  return result;
 }
--- test/crawl.test.ts
+++ test/crawl.test.ts
@@ -1,14 +1,41 @@
-import { normalizeURL } from "../src/crawl";
+import { getHeadingFromHTML, normalizeURL } from "../src/crawl";
 
-const expectedURL = "www.boot.dev/blog/path";
+describe("normalizeURL tests", () => {
+  const expectedURL = "www.boot.dev/blog/path";
 
-test.for([
-  ["https://www.boot.dev/blog/path/", expectedURL],
-  ["https://www.boot.dev/blog/path", expectedURL],
-  ["http://www.boot.dev/blog/path/", expectedURL],
-  ["http://www.boot.dev/blog/path", expectedURL],
-  ["https://www.boot.dev/blog/path/?ref=1", expectedURL],
-  ["https://www.boot.dev/blog/path/index.html", expectedURL + "/index.html"],
-])("normalizeURL(%s) -> %s", ([a, expected]) => {
-  expect(normalizeURL(a)).toBe(expected);
+  test.for([
+    ["https://www.boot.dev/blog/path/", expectedURL],
+    ["https://www.boot.dev/blog/path", expectedURL],
+    ["http://www.boot.dev/blog/path/", expectedURL],
+    ["http://www.boot.dev/blog/path", expectedURL],
+    ["https://www.boot.dev/blog/path/?ref=1", expectedURL],
+    ["https://www.boot.dev/blog/path/index.html", expectedURL + "/index.html"],
+    ["terefere", undefined],
+  ])("normalizeURL(%s) -> %s", ([a, expected]) => {
+    expect(normalizeURL(a)).toBe(expected);
+  });
+});
+
+describe("getHeadingFrom tests", () => {
+  test("getHeadingFromHTML finds first h1", () => {
+    const input =
+      "<html><body><h1>Test Title</h1><h1>Second title</h1></body></html>";
+    const expected = "Test Title";
+    const actual = getHeadingFromHTML(input);
+    expect(actual).toBe(expected);
+  });
+
+  test("getHeadingFromHTML finds h2 if no h1", () => {
+    const input = "<html><body><h2>Sub title</h2></body></html>";
+    const expected = "Sub title";
+    const actual = getHeadingFromHTML(input);
+    expect(actual).toBe(expected);
+  });
+
+  test("getHeadingFromHTML empty when no h1 and h2", () => {
+    const input = "<html><body>p>Text</p></body></html>";
+    const expected = "";
+    const actual = getHeadingFromHTML(input);
+    expect(actual).toBe(expected);
+  });
 });
